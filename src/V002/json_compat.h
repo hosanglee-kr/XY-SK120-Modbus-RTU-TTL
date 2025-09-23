c4c3610d2b02917d3543fdc1e4a9f850986b425b
hosanglee-kr <ddongl@gmail.com>
2025-09-23 18:38:18 +0900

    modified:   src/V002

--- src/V002/json_compat.h
+++ src/V002/json_compat.h
@@ -3,49 +3,38 @@
 #pragma once
 #include <ArduinoJson.h>
 
-// ArduinoJson v6와 v7을 모두 지원하기 위한 호환 계층
-#if defined(ARDUINOJSON_VERSION_MAJOR) && (ARDUINOJSON_VERSION_MAJOR >= 7)
-    using JsonDoc = JsonDocument;
+// v7 호환용 alias
+using JsonDoc = ArduinoJson::DynamicJsonDocument;
 
-    // v7에서도 용량 있는 생성자가 존재하므로 그대로 사용 가능
-    inline DeserializationError jDeserialize(JsonDoc& doc, const String& s) {
-        return deserializeJson(doc, s);
-    }
-    inline DeserializationError jDeserialize(JsonDoc& doc, const char* data, size_t len) {
-        return deserializeJson(doc, data, len);
-    }
-    inline void jSerialize(const JsonDoc& doc, String& out) {
-        serializeJson(doc, out);
-    }
-    inline void jSerialize(const JsonDoc& doc, Stream& out) {
-        serializeJson(doc, out);
-    }
-    template <typename T>
-    inline T jAs(const JsonVariantConst& v) {
-        return v.as<T>();
-    }
-    inline JsonArray jToArray(JsonDoc& d) {
-        return d.to<JsonArray>();
-    }
-#else
-    using JsonDoc = DynamicJsonDocument;
-    inline DeserializationError jDeserialize(JsonDoc& doc, const String& s) {
-        return deserializeJson(doc, s);
-    }
-    inline DeserializationError jDeserialize(JsonDoc& doc, const char* data, size_t len) {
-        return deserializeJson(doc, data, len);
-    }
-    inline void jSerialize(const JsonDoc& doc, String& out) {
-        serializeJson(doc, out);
-    }
-    inline void jSerialize(const JsonDoc& doc, Stream& out) {
-        serializeJson(doc, out);
-    }
-    template <typename T>
-    inline T jAs(const JsonVariantConst& v) {
-        return v.as<T>();
-    }
-    inline JsonArray jToArray(JsonDoc& d) {
-        return d.to<JsonArray>();
-    }
-#endif
+// 직렬화 헬퍼
+inline void jSerialize(JsonDoc& doc, String& out) {
+    out.clear();
+    serializeJson(doc, out);
+}
+
+inline bool jDeserialize(JsonDoc& doc, const String& in) {
+    DeserializationError err = deserializeJson(doc, in);
+    return err != DeserializationError::Ok;
+}
+
+// ------------------- containsKey 호환 -------------------
+// ArduinoJson v7에서는 containsKey() deprecated → obj[key].isNull() 체크 사용
+template <typename TObject>
+inline bool compatContainsKey(TObject& obj, const char* key) {
+    return !obj[key].isNull();
+}
+
+// 기존 코드 그대로 동작하도록 매크로 정의
+#define containsKey(key) compatContainsKey(*this, key)
+
+
+// ------------------- createNestedObject 호환 -------------------
+// ArduinoJson v7에서는 createNestedObject() deprecated → add<JsonObject>() 사용
+template <typename TArray>
+inline ArduinoJson::JsonObject compatCreateNestedObject(TArray& arr) {
+    return arr.add<ArduinoJson::JsonObject>();
+}
+
+// 기존 코드 그대로 동작하도록 매크로 정의
+#define createNestedObject() compatCreateNestedObject(*this)
+
